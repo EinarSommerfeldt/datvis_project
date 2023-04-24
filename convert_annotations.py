e46eba036fdc17e4c08864705904c3106aa71856
Einar Sommerfeldt <einar.sommerfeldt@gmail.com>
2023-04-24 10:15:43 +0200

    convert from xml to yolo annotations

--- convert_annotations.py
+++ convert_annotations.py
@@ -7,7 +7,7 @@ def convert_annotations_to_YOLO(folderpath):
     filelist = []
     # iterate over files in
     # that directory
-    xml_path = folderpath+"/xmls"
+    xml_path = folderpath+"/annotations/xmls"
     for filename in os.listdir(xml_path):
         f = os.path.join(xml_path, filename)
         # checking if it is a file
@@ -27,7 +27,10 @@ def convert_annotations_to_YOLO(folderpath):
             continue
         file =open(folderpath+"/labels/"+f_name+".txt", "w" )
         for obj in objects:
-            class_nr = class_to_nr[obj.findtext("name")]
+            class_name = obj.findtext("name")
+            if class_name not in class_to_nr:
+                continue
+            class_nr = class_to_nr[class_name]
 
 
             bbox = obj.find("bndbox")
@@ -42,7 +45,7 @@ def convert_annotations_to_YOLO(folderpath):
             x_center = x_min + box_width/2
             y_center = y_min + box_height/2
 
-            file.write(f"{class_nr} {x_center} {y_center} {box_width} {box_height}\n")
+            file.write(f"{class_nr} {x_center} {y_center} {box_width} {box_height}/n")
         file.close()
             
 
@@ -50,6 +53,16 @@ def convert_annotations_to_YOLO(folderpath):
 
         
 
-
-path = "C:/Users/einar/Desktop/datvis_project/RDD2022_dataset/RDD2022_released_through_CRDDC2022/RDD2022/Norway/train/annotations"
+prefix = "C:/Users/einar/Desktop/datvis_project/RDD2022_dataset/RDD2022_released_through_CRDDC2022/RDD2022/"
+path = prefix+"China_Drone/train"
+#convert_annotations_to_YOLO(path)
+path = prefix+"China_MotorBike/train"
+#convert_annotations_to_YOLO(path)
+path = prefix+"Czech/train"
+convert_annotations_to_YOLO(path)
+path = prefix+"India/train"
+convert_annotations_to_YOLO(path)
+path = prefix+"Japan/train"
+convert_annotations_to_YOLO(path)
+path = prefix+"United_States/train"
 convert_annotations_to_YOLO(path)
