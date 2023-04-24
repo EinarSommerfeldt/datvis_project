ada135df86e4527b249c749d33b4aeb5bcfa97f6
Einar Sommerfeldt <einar.sommerfeldt@gmail.com>
2023-04-24 16:17:05 +0200

    custom dataloading

---
+++ fill_image_paths.py
@@ -0,0 +1,30 @@
+import os
+
+# dataset root dir
+path = "C:/Users/einar/Desktop/datvis_project/RDD2022_dataset/RDD2022_released_through_CRDDC2022/RDD2022/"  
+dirs = ["China_Drone/train/images", 
+	"China_MotorBike/train/images", 
+	"Czech/train/images", 
+	"India/train/images", 
+	"Japan/train/images", 
+	"Norway/train/images", 
+	"United_States/train/images"] 
+dirs = ["Norway/train/images"]
+
+def fill_image_paths(train_filename, val_filename, train_prop: float):
+    train_file = open(train_filename, "w")
+    val_file = open(val_filename, "w")
+
+    for dir in dirs:
+        dir_path = path + dir
+        file_list = os.listdir(dir_path)
+        for i, filename in enumerate(file_list):
+            line = dir_path+os.sep+filename+'\n'
+            if i < train_prop*len(file_list):
+                train_file.write(line.replace("/", os.sep))
+            else:
+                val_file.write(line.replace("/", os.sep))
+
+
+
+fill_image_paths("C:/Users/einar/Desktop/datvis_project/yolo/data_info/train.txt","C:/Users/einar/Desktop/datvis_project/yolo/data_info/val.txt", 0.9)
