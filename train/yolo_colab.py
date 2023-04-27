33f544485ca7bf07ec8c0f200adcb925f70a9fc4
Einar Sommerfeldt <einar.sommerfeldt@gmail.com>
2023-04-27 14:52:29 +0200

    decrease colab workers

--- train/yolo_colab.py
+++ train/yolo_colab.py
@@ -4,4 +4,4 @@ from ultralytics import YOLO
 # Load a model
 model = YOLO_custom('/content/gdrive/MyDrive/repos/datvis_project/train/weights/32ep_fullset.pt') 
 
-model.train(data='/content/gdrive/MyDrive/repos/datvis_project/train/colab.yaml', epochs=30, imgsz=640, save_period=1, workers=8)
+model.train(data='/content/gdrive/MyDrive/repos/datvis_project/train/colab.yaml', epochs=30, imgsz=640, save_period=1, workers=4)
