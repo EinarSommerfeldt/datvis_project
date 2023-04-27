f9fb89f0c2638ac9d957675692a38a6b45d9baf7
Einar Sommerfeldt <einar.sommerfeldt@gmail.com>
2023-04-27 14:16:46 +0200

    ready colab script ?

--- train/yolo_colab.py
+++ train/yolo_colab.py
@@ -2,7 +2,6 @@ from custom_model import YOLO_custom
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO_custom('/cluster/home/einarjso/datvis_project/runs/detect/train3/weights/epoch32.pt')  # load an official model
-#model = YOLO('yolov8n.pt')
+model = YOLO_custom('/content/gdrive/MyDrive/repos/datvis_project/train/weights/32ep_fullset.pt') 
 
-model.train(data='/cluster/home/einarjso/datvis_project/yolo/rdd2022.yaml', epochs=100, imgsz=640, save_period=1, workers=4)
+model.train(data='/content/gdrive/MyDrive/repos/datvis_project/train/colab.yaml', epochs=30, imgsz=640, save_period=1, workers=4)
